7e86fb5a6b3fcdbb99a54eae2b3e2f8ceca7efdb
manihateu <nik852.nikita@mail.ru>
2022-11-28 00:25:07 +0400

    lab13

---
+++ лаб13/lab13/Form1.Designer.cs
@@ -0,0 +1,390 @@
+﻿namespace lab13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(214, 281);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(401, 281);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(589, 281);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Количество итераций";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(398, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Количество итераций";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(586, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Количество итераций";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Заполнить массив";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(403, 307);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Отсортировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox5.Location = new System.Drawing.Point(35, 30);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(133, 225);
+            this.textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox6.Location = new System.Drawing.Point(214, 30);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(133, 225);
+            this.textBox6.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox7.Location = new System.Drawing.Point(401, 30);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(133, 225);
+            this.textBox7.TabIndex = 16;
+            // 
+            // textBox8
+            // 
+            this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox8.Location = new System.Drawing.Point(580, 30);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(133, 225);
+            this.textBox8.TabIndex = 17;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(35, 347);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 18;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(35, 373);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "найти";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 333);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Найти элемент массива";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(141, 350);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 21;
+            // 
+            // textBox10
+            // 
+            this.textBox10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox10.Location = new System.Drawing.Point(746, 30);
+            this.textBox10.Multiline = true;
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(133, 225);
+            this.textBox10.TabIndex = 22;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(743, 265);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(116, 13);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "Количество итераций";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(746, 281);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 23;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(626, 353);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 28;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(517, 336);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(189, 13);
+            this.label9.TabIndex = 27;
+            this.label9.Text = "Найти элемент методом дихотомии";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(520, 415);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 26;
+            this.button4.Text = "найти";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(520, 350);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 20);
+            this.textBox12.TabIndex = 25;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(520, 389);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(100, 20);
+            this.textBox13.TabIndex = 29;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(517, 373);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(94, 13);
+            this.label10.TabIndex = 30;
+            this.label10.Text = "Кол-во итераций:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(211, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Сортировка пузырьком";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(398, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(116, 13);
+            this.label11.TabIndex = 32;
+            this.label11.Text = "Сортировка выбором";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(577, 9);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(113, 13);
+            this.label12.TabIndex = 33;
+            this.label12.Text = "Быстрая сортировка";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(743, 14);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(103, 13);
+            this.label13.TabIndex = 34;
+            this.label13.Text = "Сортировка Шелла";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 450);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = "Сортировки";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ лаб13/lab13/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lab13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int iteration = 0;
+        int iterationOfDihotomy = 0;
+        void BubbleSort(ref int[] Array)
+        {
+            for (int i = 0; i < Array.Length - 1; i++) {
+                for (int j = i + 1; j < Array.Length; j++)
+                    if (Array[i] > Array[j])
+                    {
+                        int t = Array[i];
+                        Array[i] = Array[j];
+                        Array[j] = t;
+                    }
+                iteration += 1;
+            }
+        }
+
+        void SelectionSort(ref int[] Array)
+        {
+
+            for (int i = 0; i < Array.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < Array.Length; j++)
+                    if (Array[j] < Array[min])
+                        min = j;
+                        
+                        if (min != i)
+                        {
+                            int t = Array[i];
+                            Array[i] = Array[min];
+                            Array[min] = t;
+                            iteration += 1;
+                        }
+            }
+        }
+
+        void SortArray(ref int[] array, int leftIndex, int rightIndex)
+        {
+            var i = leftIndex;
+            var j = rightIndex;
+            var pivot = array[leftIndex];
+            while (i <= j)
+            {
+                while (array[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (array[j] > pivot)
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                    iteration += 1;
+                }
+            }
+
+            if (leftIndex < j)
+                SortArray(ref array, leftIndex, j);
+            if (i < rightIndex)
+                SortArray(ref array, i, rightIndex);
+            
+        }
+
+        void ShellSort(ref int[] array, int size)
+        {
+            for (int interval = size / 2; interval > 0; interval /= 2)
+            {
+                for (int i = interval; i < size; i++)
+                {
+                    var currentKey = array[i];
+                    var k = i;
+                    while (k >= interval && array[k - interval] > currentKey)
+                    {
+                        iteration += 1;
+                        array[k] = array[k - interval];
+                        k -= interval;
+                    }
+                    array[k] = currentKey;
+                }
+            }
+            
+        }
+
+        int IndexOf(ref int[] Array, int Value)
+        {
+            
+            for (int i = 0; i < Array.Length; i++)
+                // Нашли нужное значение? Возвращаем его индекс
+                if (Array[i] == Value)
+                    return i;
+            
+            return -1;
+        }
+
+        int IndexOfDihotomy(ref int[] Array, int Value, int Left, int Right)
+        {
+            iterationOfDihotomy += 1;
+            int x = (Left + Right) / 2;
+            if (Array[x] == Value)
+                return x;
+            if ((x == Left) || (x == Right))
+                return -1;
+            if (Array[x] < Value)
+                return IndexOfDihotomy(ref Array, Value, x, Right);
+            else
+                return IndexOfDihotomy(ref Array, Value, Left, x);
+        }
+
+
+
+
+        int[] numbers = new int[100];
+        int[] numberstosort = new int[100];
+        int[] numberstodihotomy = new int[100];
+        private void button1_Click(object sender, EventArgs e)
+            {
+            
+                textBox5.Text = "";
+                Array.Clear(numbers, 0, 100);
+                Random rand = new Random();
+                for (int y = 0; y < 100; y++)
+                {
+                    numbers[y] = rand.Next(1,100);
+                    numberstosort[y] = numbers[y];
+                    numberstodihotomy[y] = numbers[y];
+                    textBox5.Text += "[" + numbers[y].ToString() + "]";
+                }
+                BubbleSort(ref numberstodihotomy);
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            iteration = 0;
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox10.Text = "";
+            BubbleSort(ref numberstosort);
+            for (int y = 0; y < 100; y++)
+            {
+                textBox6.Text += "[" + numberstosort[y].ToString() + "]";
+                numberstosort[y] = numbers[y];
+            }
+            textBox2.Text = iteration.ToString();
+            iteration = 0;
+            SelectionSort(ref numberstosort);
+            for (int y = 0; y < 100; y++)
+            {
+                textBox7.Text += "[" + numberstosort[y].ToString() + "]";
+                numberstosort[y] = numbers[y];
+            }
+            textBox3.Text = iteration.ToString();
+            iteration = 0;
+            SortArray(ref numberstosort, 0, 99);
+            for (int y = 0; y < 100; y++)
+            {
+                textBox8.Text += "[" + numberstosort[y].ToString() + "]";
+                numberstosort[y] = numbers[y];
+            }
+            textBox4.Text = iteration.ToString();
+            iteration = 0;
+            ShellSort(ref numberstosort,100);
+            for (int y = 0; y < 100; y++)
+            {
+                textBox10.Text += "[" + numberstosort[y].ToString() + "]";
+                numberstosort[y] = numbers[y];
+            }
+            textBox11.Text = iteration.ToString();
+            iteration = 0;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label6.Text = "";
+            try
+            {
+                
+                int value = int.Parse(textBox9.Text);
+                int index = IndexOf(ref numbers, value);
+                if (index != -1)
+                {
+                    label6.Text = "Элемент найден!";
+                }
+                else
+                {
+                    label6.Text = "Элемент не найден!";
+                }
+            }
+            catch { }
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            //label8
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label8.Text = "";
+            try
+            {
+                int value = int.Parse(textBox12.Text);
+                int index = IndexOfDihotomy(ref numberstodihotomy, value, 0, 99);
+                if (index != -1)
+                {
+                    label8.Text = "Элемент найден!";
+                }
+                else
+                {
+                    label8.Text = "Элемент не найден!";
+                }
+                textBox13.Text = iterationOfDihotomy.ToString();
+                iterationOfDihotomy = 0;
+            }
+            catch { }
+        }
+    }
+}
+
